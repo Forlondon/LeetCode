c87f6c4ce6a5a27594d6cd25a72412d6ac73de75
luozh <978309427@qq.com>
2022-02-15 17:58:19 +0800

    更新

--- src/leetcode/editor/cn/P1TwoSum.java
+++ src/leetcode/editor/cn/P1TwoSum.java
@@ -16,25 +16,42 @@
 
 package leetcode.editor.cn;
 
+import java.util.HashMap;
+
 //Java：两数之和
 public class P1TwoSum {
     public static void main(String[] args) {
         Solution solution = new P1TwoSum().new Solution();
         // TEST
-        int[] a = solution.twoSum(new int[]{1,2,3,4},5);
+        int[] a = solution.twoSum2(new int[]{1, 2, 3, 4}, 5);
         for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
         }
+
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        //暴力循环遍历
         public int[] twoSum(int[] nums, int target) {
             for (int i = 0; i < nums.length; i++) {
                 for (int j = i + 1; j < nums.length; j++) {
                     if (nums[j] == target - nums[i]) {
                         return new int[]{i, j};
                     }
+                }
+            }
+            return new int[]{0, 0};
+        }
+
+        //用hash表，key存数组的值，value存数组的下标
+        public int[] twoSum2(int[] nums, int target) {
+            HashMap<Integer, Integer> hashMap = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (hashMap.containsKey(target - nums[i])) {
+                    return new int[]{hashMap.get(target - nums[i]), i};
+                } else {
+                    hashMap.put(nums[i], i);
                 }
             }
             return new int[]{0, 0};
